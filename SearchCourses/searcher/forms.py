0a279643676168f6dab4030f620863d0d630789e
caio-ggomes <caio.graca@gmail.com>
2020-08-09 14:06:05 -0300

    Tradução das field_tags dpo formulario para Portugues

--- SearchCourses/searcher/forms.py
+++ SearchCourses/searcher/forms.py
@@ -7,11 +7,16 @@ class SearchForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(SearchForm, self).__init__(*args, **kwargs)
         self.fields['subject'].widget = forms.TextInput()
+        self.fields['subject'].label = 'Matéria'
         self.fields['platform'].widget = forms.Select(choices=platform_choices)
+        self.fields['platform'].label = 'Plataforma'
         self.fields['difficulty'].widget = forms.Select(choices=difficulty_choices)
+        self.fields['difficulty'].label = 'Dificuldade'
         self.fields['duration'].widget = forms.Select(choices=duration_choices)
+        self.fields['duration'].label = 'Duração'
         self.fields['language'].widget = forms.Select(choices=language_choices)
-    
+        self.fields['language'].label = 'Idioma'
+
     class Meta:
         model = Search
         fields = ['subject', 'platform', 'difficulty', 'duration', 'language']
